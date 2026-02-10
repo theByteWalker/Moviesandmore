39d030386ed9504c9efce35721d64df92f0f53b3
Allay Sahoo <allay.sahoo@technogise.com>
2026-02-11 00:05:25 +0530

    chore(MovieApiService): added movie mappercode with dto.

---
+++ app/src/main/java/com/example/moviesandmore/data/movie/MovieMapper.kt
@@ -0,0 +1,15 @@
+package com.example.moviesandmore.data.movie
+
+import com.example.moviesandmore.domain.movie.Movie
+
+class MovieMapper {
+    fun toDomain(dto: MovieDto): Movie {
+        return Movie(
+            name = dto.primaryTitle
+        )
+    }
+
+    fun toDomainList(dtos: List<MovieDto>): List<Movie> {
+        return dtos.map { toDomain(it) }
+    }
+}
