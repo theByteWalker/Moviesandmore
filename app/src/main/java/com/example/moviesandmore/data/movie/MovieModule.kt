0bec88889e1fd06d6798df89c848d248d7a4ce16
Allay Sahoo <allay.sahoo@technogise.com>
2026-02-11 00:06:04 +0530

    chore(MovieApiService): added factory module for dependency injection.

---
+++ app/src/main/java/com/example/moviesandmore/data/movie/MovieModule.kt
@@ -0,0 +1,35 @@
+package com.example.moviesandmore.data.movie
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class MovieModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.imdbapi.dev/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieApiService(retrofit: Retrofit): MovieApiService {
+        return retrofit.create(MovieApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieMapper(): MovieMapper {
+        return MovieMapper()
+    }
+}
