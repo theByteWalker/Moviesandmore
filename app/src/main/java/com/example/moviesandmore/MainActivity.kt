83d367dbe1dcf32228a5aff46fc8649dd5f8ec79
Allay Sahoo <allay.sahoo@technogise.com>
2026-02-11 01:25:54 +0530

    feat(ui): integrate MovieSearchScreen in MainActivity
    - Display MovieSearchScreen on app launch
    - Add import for MovieSearchScreen composable
    - Update preview to show search screen

--- app/src/main/java/com/example/moviesandmore/MainActivity.kt
+++ app/src/main/java/com/example/moviesandmore/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.moviesandmore.presentation.movie.MovieSearchScreen
 import com.example.moviesandmore.ui.theme.MoviesAndMoreTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,7 +17,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MoviesAndMoreTheme {
-
+                MovieSearchScreen()
             }
         }
     }
@@ -26,5 +27,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     MoviesAndMoreTheme {
+        MovieSearchScreen()
     }
 }
