6cbc231559a21e9882935397b31b32ea3c2c7115
Allay Sahoo <allay.sahoo@technogise.com>
2026-02-10 17:04:07 +0530

    chore(MovieRepositoryImpl): Added code for throwing an error when input is a invalid movie title.

--- app/src/main/java/com/example/moviesandmore/data/movie/MovieRepositoryImpl.kt
+++ app/src/main/java/com/example/moviesandmore/data/movie/MovieRepositoryImpl.kt
@@ -5,6 +5,9 @@ import com.example.moviesandmore.domain.movie.MovieRepository
 
 class MovieRepositoryImpl: MovieRepository {
     override fun searchMovieByTitle(movieTitle: String): List<Movie>{
+        if (movieTitle.isEmpty()) {
+            throw IllegalArgumentException("Invalid input")
+        }
         return listOf(Movie("Avengers"), Movie("Avenger: Endgame"))
     }
 }
