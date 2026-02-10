fede7d8d3ca1e3e366fab04d0c81f08c79457585
Allay Sahoo <allay.sahoo@technogise.com>
2026-02-11 00:02:56 +0530

    chore(MovieRepositoryImpl): added api service call to movie repository impl.

--- app/src/main/java/com/example/moviesandmore/data/movie/MovieRepositoryImpl.kt
+++ app/src/main/java/com/example/moviesandmore/data/movie/MovieRepositoryImpl.kt
@@ -2,12 +2,16 @@ package com.example.moviesandmore.data.movie
 
 import com.example.moviesandmore.domain.movie.Movie
 import com.example.moviesandmore.domain.movie.MovieRepository
+import javax.inject.Inject
 
-class MovieRepositoryImpl: MovieRepository {
+class MovieRepositoryImpl @Inject constructor(private val movieApiService: MovieApiService, private val movieMapper: MovieMapper) : MovieRepository {
     override fun searchMovieByTitle(movieTitle: String): List<Movie>{
         if (movieTitle.isEmpty()) {
             throw IllegalArgumentException("Invalid input")
         }
-        return listOf(Movie("Avengers"), Movie("Avenger: Endgame"))
+        val response = movieApiService.getMoviesByTitle(movieTitle)
+        val movieApiResponse = response.body()
+        val movieDtos = movieApiResponse?.titles ?: emptyList()
+        return movieMapper.toDomainList(movieDtos)
     }
 }
