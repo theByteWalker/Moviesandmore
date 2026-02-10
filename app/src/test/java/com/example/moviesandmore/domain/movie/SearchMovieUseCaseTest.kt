0799eae630eecb20ca4112e9db665a9a77a320db
Allay Sahoo <allay.sahoo@technogise.com>
2026-02-10 16:00:04 +0530

    chore(SearchMovieUseCase): Added test for invalid input string.

--- app/src/test/java/com/example/moviesandmore/domain/movie/SearchMovieUseCaseTest.kt
+++ app/src/test/java/com/example/moviesandmore/domain/movie/SearchMovieUseCaseTest.kt
@@ -1,14 +1,22 @@
 package com.example.moviesandmore.domain.movie
 
+import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNotNull
 import org.junit.Test
 
 class SearchMovieUseCaseTest {
+    val useCase = SearchMovieUseCase()
 
     @Test
     fun shouldBeAbleToSearchForMovieByMovieTitle() {
-        val mockUseCase = SearchMovieUseCase()
-        val result = mockUseCase.execute("Avengers")
+        val result = useCase.execute("Avengers")
         assertNotNull(result)
+    }
+
+    @Test
+    fun shouldReturnErrorIfInputStringISNotValid() {
+        val erroResponse = "Invalid input"
+        val result = useCase.execute("")
+        assertEquals(erroResponse,result)
     }
 }
