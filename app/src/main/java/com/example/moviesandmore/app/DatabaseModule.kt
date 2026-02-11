205b8f3be67799a07cbb847a122036eb230d0a7d
Allay Sahoo <allay.sahoo@technogise.com>
2026-02-11 15:56:41 +0530

    chore(dependencies): added app database into factory for DI.

---
+++ app/src/main/java/com/example/moviesandmore/app/DatabaseModule.kt
@@ -0,0 +1,27 @@
+package com.example.moviesandmore.app
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "app_database"
+        ).build()
+    }
+}
