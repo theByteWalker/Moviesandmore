2af29f67db6dfd6d36b855fad8f3ceeed4287d7f
Allay Sahoo <allay.sahoo@technogise.com>
2026-02-12 23:30:30 +0530

    feat(ui): integrate navigation in MainActivity

--- app/src/main/java/com/example/moviesandmore/MainActivity.kt
+++ app/src/main/java/com/example/moviesandmore/MainActivity.kt
@@ -4,9 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.moviesandmore.presentation.favorites.FavoritesScreen
 import com.example.moviesandmore.presentation.movie.MovieSearchScreen
+import com.example.moviesandmore.presentation.moviedetails.MovieDetails
+import com.example.moviesandmore.presentation.navigation.Screen
 import com.example.moviesandmore.ui.theme.MoviesAndMoreTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,16 +35,80 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MoviesAndMoreTheme {
-                MovieSearchScreen()
+                val navController = rememberNavController()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        SimpleBottomBar(
+                            onFavoritesClick = {
+                                navController.navigate(Screen.Favorites.route) {
+                                    launchSingleTop = true
+                                }
+                            },
+                            onSearchClick = {
+                                navController.navigate(Screen.Search.route) {
+                                    launchSingleTop = true
+                                    popUpTo(Screen.Search.route) { inclusive = true }
+                                }
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.Search.route,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(bottom = innerPadding.calculateBottomPadding())
+                    ) {
+                        composable(Screen.Search.route) {
+                            MovieSearchScreen(
+                                onCardClick = { movie ->
+                                    navController.navigate(Screen.MovieDetail.createRoute(movie.titleId))
+                                }
+                            )
+                        }
+                        composable(Screen.Favorites.route) {
+                            FavoritesScreen(
+                                onCardClick = {
+                                    navController.navigate(Screen.MovieDetail.route)
+                                }
+                            )
+                        }
+                        composable(Screen.MovieDetail.route, content = { backStackEntry ->
+                            val movieId = backStackEntry.arguments?.getString("movieId")
+                            MovieDetails(movieId)
+                        })
+                    }
+                }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MoviesAndMoreTheme {
-        MovieSearchScreen()
+fun SimpleBottomBar(
+    onFavoritesClick: () -> Unit,
+    onSearchClick: () -> Unit
+) {
+    BottomAppBar {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            IconButton(onClick = onSearchClick) {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = "Search"
+                )
+            }
+
+            IconButton(onClick = onFavoritesClick) {
+                Icon(
+                    imageVector = Icons.Outlined.Favorite,
+                    contentDescription = "Favorites"
+                )
+            }
+        }
     }
 }
