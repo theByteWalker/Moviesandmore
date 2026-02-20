4decdc42539ad29422a3f78fcdde2b9ef6e3017b
Allay Sahoo <allay.sahoo@technogise.com>
2026-02-20 17:38:45 +0530

    refactor(vm): use GetPopularMoviesPagerUseCase in PopularMoviesViewModel

--- app/src/main/java/com/example/moviesandmore/presentation/popularmovies/PopularMoviesViewModel.kt
+++ app/src/main/java/com/example/moviesandmore/presentation/popularmovies/PopularMoviesViewModel.kt
@@ -4,17 +4,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import com.example.moviesandmore.domain.movie.GetPopularMoviesPagerUseCase
 import com.example.moviesandmore.domain.movie.Movie
-import com.example.moviesandmore.domain.movie.MovieRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
 class PopularMoviesViewModel @Inject constructor(
-    movieRepository: MovieRepository
+    getPopularMoviesPagerUseCase: GetPopularMoviesPagerUseCase
 ) : ViewModel() {
 
     val moviesPagingFlow: Flow<PagingData<Movie>> =
-        movieRepository.getPopularMoviesPager().cachedIn(viewModelScope)
+        getPopularMoviesPagerUseCase().cachedIn(viewModelScope)
 }
