7e0b47ba2f72b2aa4094053ff7c19a08fc0ee8c8
Allay Sahoo <allay.sahoo@technogise.com>
2026-02-20 15:35:24 +0530

    refactor(ui): extract common TopBar component

---
+++ app/src/main/java/com/example/moviesandmore/presentation/navigation/TopBar.kt
@@ -0,0 +1,20 @@
+package com.example.moviesandmore.presentation.navigation
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(title: String) {
+    TopAppBar(
+        title = { Text(text = title) },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    )
+}
