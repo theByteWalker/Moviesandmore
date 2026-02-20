b2b178a4348c24d2421eef868ee6e5bdfa13c712
Allay Sahoo <allay.sahoo@technogise.com>
2026-02-20 15:35:05 +0530

    feat(nav): add PopularMovies screen and route

--- app/src/main/java/com/example/moviesandmore/presentation/navigation/Screen.kt
+++ app/src/main/java/com/example/moviesandmore/presentation/navigation/Screen.kt
@@ -4,6 +4,7 @@ sealed class Screen(val route: String) {
     object Search : Screen("search")
     object Favorites : Screen("favorites")
     object Album : Screen("album")
+    object PopularMovies : Screen("popularMovies")
     data object MovieDetail : Screen("movieDetail/{movieId}"){
         fun createRoute(movieId: String) = "movieDetail/$movieId"
     }
---
+++ app/src/main/java/com/example/moviesandmore/presentation/popularmovies/PopularMoviesScreen.kt
@@ -0,0 +1,27 @@
+package com.example.moviesandmore.presentation.popularmovies
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.moviesandmore.presentation.navigation.TopBar
+
+@Composable
+fun PopularMoviesScreen() {
+    Scaffold(
+        topBar = { TopBar("Popular Movies") }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Popular Movies")
+        }
+    }
+}
