6ab0bd4d083f5e212874fbd3b0f033ebe36e2b70
Allay Sahoo <allay.sahoo@technogise.com>
2026-02-20 17:39:01 +0530

    refactor(domain): comment out GetAllPopularMoviesUseCase in favor of Paging 3

--- app/src/main/java/com/example/moviesandmore/domain/movie/GetAllPopularMoviesUseCase.kt
+++ app/src/main/java/com/example/moviesandmore/domain/movie/GetAllPopularMoviesUseCase.kt
@@ -1,9 +1,12 @@
 package com.example.moviesandmore.domain.movie
 
-import javax.inject.Inject
+//import javax.inject.Inject
 
-class GetAllPopularMoviesUseCase @Inject constructor(private val movieRepository: MovieRepository) {
-    suspend fun execute(): List<Movie> {
-        return movieRepository.getAllPopularMovies()
-    }
-}
+// Replaced by GetPopularMoviesPagerUseCase which returns Flow<PagingData<Movie>>
+// for Paging 3 integration. This one-shot approach doesn't support pagination.
+
+//class GetAllPopularMoviesUseCase @Inject constructor(private val movieRepository: MovieRepository) {
+//    suspend fun execute(): List<Movie> {
+//        return movieRepository.getAllPopularMovies()
+//    }
+//}
