e157f4c8ec26bbb9d973adb0791674ee0ec70c88
Allay Sahoo <allay.sahoo@technogise.com>
2026-02-24 15:27:25 +0530

    refactor(domain): add empty movieId validation to GetMovieByIdUseCase

--- app/src/main/java/com/example/moviesandmore/domain/movie/GetMovieByIdUseCase.kt
+++ app/src/main/java/com/example/moviesandmore/domain/movie/GetMovieByIdUseCase.kt
@@ -4,6 +4,9 @@ import javax.inject.Inject
 
 class GetMovieByIdUseCase @Inject constructor(private val movieRepository: MovieRepository) {
     suspend fun execute(movieId: String): Movie {
+        if (movieId.isEmpty()) {
+            throw IllegalArgumentException("Invalid movie ID")
+        }
         return movieRepository.getMovieById(movieId)
     }
 }
